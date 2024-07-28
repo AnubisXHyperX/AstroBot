66c6bbc12cc7299e5aa2a841c9462ed2351270fa
Yitzhack Yitzhack <yitzhack165@windowslive.com>
2024-07-28 23:42:19 +0300

    fix commented guildMemberAdd

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,13 +1,13 @@
 // module.exports = {
 //   name: 'guildMemberAdd',
 //   async execute (member) {
-//     const welcomeRole = await member.guild.roles.cache.find(
-//       role => role.name === 'Verified'
-//     )
-//     await member.roles.add(welcomeRole)
+//     // const welcomeRole = await member.guild.roles.cache.find(
+//     //   role => role.name === 'Verified'
+//     // )
+//     // await member.roles.add(welcomeRole)
 
 //     const welcomeChannel = await member.guild.channels.cache.find(
-//       channel => channel.name === 'welcome'
+//       channel => channel.name === 'lobby'
 //     )
 //     await welcomeChannel.fetch()
 //     welcomeChannel.send(`Welcome to the server ${member.user}`)
